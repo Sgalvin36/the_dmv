707566081286b632085aa604adf3aa12d6ee6bbe
Shane Galvin <Sgalvin36@gmail.com>
2024-07-12 14:15:24 -0600

    Add test suite for renew_license method

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -209,6 +209,40 @@ RSpec.describe Dmv do
     end
 
     describe '#renew a drivers license' do
+      before (:each) do
+        @facility_1.add_service('Written Test')
+        @facility_1.add_service('Road Test')
+      end
+
+      it 'can renew license' do
+        expect(@facility_1.services).to eq ['Written Test', 'Road Test']
+        @facility_1.add_service('Renew License')
+        expect(@facility_1.services).to include 'Renew License'
+        expect(@facility_1.services.count).to eq 3
+
+      end
+
+      it 'only renew if written and license are true in registrant data' do
+        expect(@registrant_1.license_data[:renewed]).to eq false
+        expect(@facility_1.renew_drivers_license(@registrant_1)).to eq false
+
+        @facility_1.administer_written_test(@registrant_1)
+        @facility_1.administer_road_test(@registrant_1)
+        expect(@registrant_1.license_data[:written]).to eq true
+        expect(@registrant_1.license_data[:license]).to eq true
+
+        expect(@facility_1.renew_drivers_license(@registrant_1)).to eq false
+        @facility_1.add_service('Renew License')
+        expect(@facility_1.renew_drivers_license(@registrant_1)).to eq true        
+      end
+
+      it 'updates the registrants license info if they renew' do
+        @facility_1.add_service('Road Test')
+        @facility_1.administer_written_test(@registrant_1)
+        @facility_1.administer_road_test(@registrant_1)
+        @facility_1.renew_drivers_license(@registrant_1)
+        expect(@registrant_1.license_data[:renewed]).to eq true
+      end
     end
   end
 end
