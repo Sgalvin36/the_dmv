228d024525132045944695d10451131222b09945
Shane Galvin <Sgalvin36@gmail.com>
2024-07-13 11:24:26 -0600

    Add filter functionality to DMV_setup

--- spec/DMV_setup_spec.rb
+++ spec/DMV_setup_spec.rb
@@ -11,5 +11,21 @@ RSpec.describe DMVSetup do
         it 'can initialize' do
             expect(@DMV).to be_an_instance_of(DMVSetup)
         end
+
+        it 'starts with no facilities' do
+            expect(@DMV.facilities).to eq []
+        end
+
+        it 'can filter data' do
+            facility_1 = {name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue', address_2: 'Suite 315', city: 'Denver', state: 'CO', zip: '80211', phone: '(720) 865-4600'}
+            expect(@DMV.filter_data(facility_1)).to be_an_instance_of Hash
+        end
+
+        it 'arranges all address info into single string' do
+            facility_1 = {name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue', address_2: 'Suite 315', city: 'Denver', state: 'CO', zip: '80211', phone: '(720) 865-4600'}
+            sorted_data = @DMV.filter_data(facility_1)
+
+            expect(sorted_data[:address]).to eq("3698 W. 44th Avenue Suite 315 Denver CO 80211")
+        end
     end
 end
